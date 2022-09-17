68fa74e7236b5eeede164101d16e7b20d09abb55
navitha <navithareddy112@gmail.com>
2022-09-17 21:59:27 +0530

    [navitha] UC3InsertStringSort

---
+++ Algorithms/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("choose option 3.insertsort");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    String s = "alkasingh";
+
+                    Console.WriteLine(s);
+                    break;
+                default:
+                    Console.WriteLine("enter wrong choice");
+                    break;
+
+
+                    Console.ReadLine();
+            }
+        }
+    }
+}
+    
---
+++ Algorithms/UC3InsertStringSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    internal class UC3InsertStringSort
+    {
+        static int MAX_CHAR = 26;
+        
+        public void sortString(string str)
+        {
+            int []charCount = new int[MAX_CHAR];
+            for(int i=0;i< str.Length; i++)
+            {
+                charCount[str[i] - 'a']++;
+            }
+            for (int i = MAX_CHAR - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < charCount[i]; j++)
+                {
+                    Console.Write((char)('a' + i));
+                }
+            }
+        }
+    }
+}
